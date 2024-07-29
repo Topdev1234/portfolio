20e5dd3d5603f1f69f63d34d3e9db96339d68cc5
warner0616 <warner0616@gmail.com>
2024-07-29 02:17:33 -0400

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
 };
 
 module.exports = nextConfig;
